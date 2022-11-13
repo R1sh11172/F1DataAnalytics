8a8401a14b82085aeeb68da04288495c2d47d422
NithilB <nithilb147@gmail.com>
2022-11-12 23:00:11 -0500

    Second

--- Helloworld.py
+++ Helloworld.py
@@ -6,16 +6,15 @@ def get_dataframe(csvFile="races.csv"):
   return pd.read_csv(csvFile)
 
 
-#import pandas as pd
 #my_csv = pd.read_csv(filename)
 #column = my_csv.column_name
 # you can also use my_csv['column_name']
-
 
 def get_driver_lap_times():
   df = get_dataframe()
   minidf = df[['raceId', 'year', 'name']]
   print(minidf.raceId)
+  print(minidf.year)
   pass
 
 get_driver_lap_times()
